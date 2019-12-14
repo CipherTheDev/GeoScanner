9cbf50b47c8d906c18623fbb040c391798c5061e
CipherTheDev <19726555+pastaboy2131@users.noreply.github.com>
2019-12-14 23:02:01 +1100

    Updated Docs

--- pygeoip/util.py
+++ pygeoip/util.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2010-2014 Jennifer Ennis, William Tisäter.
+Copyright (c) 2010-2019 Jennifer Ennis, William Tisäter, Parsa Zarrin.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
@@ -14,6 +14,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+
+You should also achnoledge the authors of this free API.
 """
 
 import socket
